8d117ea7fb569d809b7545512e0ead64cde56718
bagas <bagasab11@gmail.com>
2024-05-09 21:08:54 +0700

    table

--- assets/icons/index.tsx
+++ assets/icons/index.tsx
@@ -2,10 +2,19 @@ import { IconFilledEye } from './iconFilledEye'
 import { IconFilledEyeSlash } from './iconFilledEyeSlash'
 import IconClose from './iconClose'
 import IconHamburger from './iconHamburger'
-
+import { ChevronDownIcon } from './iconChevronDown'
+import { IconDelete } from './iconDelete'
+import { IconEdit } from './iconEdit'
+import { PlusIcon } from './iconPlus'
+import { SearchIcon } from './iconSearch'
 export { 
     IconFilledEye,
     IconFilledEyeSlash,
     IconClose,
     IconHamburger,
+    ChevronDownIcon,
+    IconDelete,
+    IconEdit,
+    PlusIcon,
+    SearchIcon
 }
---
+++ utils/columnsTable/dataUser.tsx
@@ -0,0 +1,10 @@
+const columns = [
+    {name: "ID", uid: "id", sortable: true},
+    {name: "USERNAME", uid: "username", sortable: true},
+    {name: "ROLE", uid: "role", sortable: true},
+    {name: "PASSWORD", uid: "password", sortable: true},
+    {name: "ACTIONS", uid: "actions"},
+  ];
+  
+  export {columns};
+  
